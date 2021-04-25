411251bcd6110739bae1e4c676f528a3714ae2d5
Ekechukwu David <63519479+davideke1@users.noreply.github.com>
2021-04-25 20:00:41 +0100

    update

--- src/settings.py
+++ src/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = [
-    BASE_DIR / "blog/static"
+    BASE_DIR / "static"
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
